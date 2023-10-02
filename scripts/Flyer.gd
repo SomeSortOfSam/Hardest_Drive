ef57e21d5ebfa40b184907874d0a09cead51ec9d
SomeSortOfSam <57550537+SomeSortOfSam@users.noreply.github.com>
2023-10-02 00:41:25 -0500

    Added flyer

---
+++ scripts/Flyer.gd
@@ -0,0 +1,9 @@
+extends "res://scripts/enemy.gd"
+
+func _physics_process(delta):
+	var new_velocity: Vector2 = to_local(player.global_position) 
+	new_velocity = new_velocity.normalized()
+	new_velocity = new_velocity * movement_speed
+	velocity = new_velocity
+	move_and_slide()
+	face_target()
