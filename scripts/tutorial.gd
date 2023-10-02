46e4e4b49b71fbe824a00d69ecbdc306ea39db2a
Hederarch <66890769+Hederarch@users.noreply.github.com>
2023-10-01 19:12:22 -0500

    increased tutorial pulls

--- scripts/tutorial.gd
+++ scripts/tutorial.gd
@@ -13,15 +13,15 @@ signal player_reset_screen_enabled
 signal done
 
 func _unhandled_input(event):
-	if ((triggered_count < 2 or (triggered_count > 2 and triggered_count <= 4))
+	if ((triggered_count < 2 or (triggered_count > 2 and triggered_count <= 6))
 	 and event is InputEventMouseButton and\
 	 event.button_index == MOUSE_BUTTON_LEFT and event.is_pressed()) or\
 	(triggered_count == 2 and
 	(event.is_action_pressed("player_up") or event.is_action_pressed("player_down") or \
 	event.is_action_pressed("player_left") or event.is_action_pressed("player_right"))) or \
-	(triggered_count == 5 and (event is InputEventMouseButton and\
+	(triggered_count == 7 and (event is InputEventMouseButton and\
 	 event.button_index == MOUSE_BUTTON_RIGHT and event.is_pressed())) or\
-	(triggered_count == 6 and event.is_action_pressed("reset_screen") ):
+	(triggered_count == 8 and event.is_action_pressed("reset_screen") ):
 		triggered_count += 1
 		if triggered_count == 1:
 			swap_banner_to(2)
@@ -36,14 +36,14 @@ func _unhandled_input(event):
 		if triggered_count == 4:
 			swap_banner_to(2)
 			animator.play("pull")
-		if triggered_count == 5:
+		if triggered_count == 7:
 			swap_banner_to(3)
 			swap_sprite_to("unhook")
-		if triggered_count == 6:
+		if triggered_count == 8:
 			swap_banner_to(4)
 			swap_sprite_to("space")
 			player_reset_screen_enabled.emit()
-		if triggered_count >= 7:
+		if triggered_count >= 9:
 			banner_mover.play("BannerOut")
 			sprite_mover.play("SpriteOut")
 			done.emit()
