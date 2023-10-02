a29ae27a27c50fa5bb4128070984af9df3c10a52
SomeSortOfSam <57550537+SomeSortOfSam@users.noreply.github.com>
2023-10-02 01:11:02 -0500

    Added whir to box

--- scenes/grapple_point.gd
+++ scenes/grapple_point.gd
@@ -1,6 +1,8 @@
 extends StaticBody2D
 
 @onready var animator :AnimationPlayer = $AnimationPlayer
+@onready var audio : AudioStreamPlayer2D = $AudioStreamPlayer2D
 
 func _on_player_pull_requested(_direction,_player):
 	animator.play("HitShine")
+	audio.play()
