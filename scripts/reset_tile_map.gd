8729a6d24e08d137e02bdd853cda5380a68415d5
SomeSortOfSam <57550537+SomeSortOfSam@users.noreply.github.com>
2023-10-01 20:35:54 -0500

    Made enemy stop interacting with itself and other enemies

--- scripts/reset_tile_map.gd
+++ scripts/reset_tile_map.gd
@@ -3,4 +3,4 @@ extends TileMap
 @onready var player = $Player
 
 func _on_player_reset_position_requested():
-	player.position = map_to_local(get_used_cells(0).pick_random()) + Vector2(tile_set.tile_size)/2
+	player.position = map_to_local(get_used_cells(0).pick_random())
